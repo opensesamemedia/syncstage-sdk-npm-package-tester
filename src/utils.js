7a27ccc23a76aa9ef9c4c99f611591b31298f60f
ZygusPatryk <patryk.kaczmarek@wp.eu>
2023-06-20 13:08:29 +0200

    updated snackbar messagess according to new error codes

--- src/utils.js
+++ src/utils.js
@@ -2,20 +2,18 @@ import { SyncStageSDKErrorCode } from '@opensesamemedia/syncstage-sdk-npm-packag
 import { enqueueSnackbar } from 'notistack';
 
 const syncStageErrorToMessageMap = new Map();
-
-syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.UNKNOWN_ERROR), 'Unknown error');
-syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.CONFIGURATION_ERROR), 'Configuration error');
-syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.API_ERROR), 'SyncStage service error - possibly invalid secret key');
 syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.API_UNAUTHORIZED), 'Unauthorized');
-syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.AUDIO_STREAMING_ERROR), 'Audio streaming error');
-syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.STREAM_DOES_NOT_EXIST), 'Stream does not exist');
-syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.BAD_VOLUME_VALUE), 'Incorrect volume value');
-syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.SESSION_NOT_JOINED), 'Session not joined');
-syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.AUDIO_SERVER_NOT_REACHABLE), 'Audio server not reachable');
 syncStageErrorToMessageMap.set(
   parseInt(SyncStageSDKErrorCode.DESKTOP_AGENT_COMMUNICATION_ERROR),
   'Desktop agent communication error. Please check if SyncStage desktop agent is running.',
 );
+syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.UNKNOWN_ERROR), 'Unknown error');
+syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.CONFIGURATION_ERROR), 'Configuration error');
+syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.API_ERROR), 'API error');
+syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.STREAM_DOES_NOT_EXIST), 'Stream does not exist');
+syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.BAD_VOLUME_VALUE), 'Incorrect volume value');
+syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.NO_ZONE_AVAILABLE), 'No zone availible');
+syncStageErrorToMessageMap.set(parseInt(SyncStageSDKErrorCode.NO_STUDIO_SERVER_AVAILABLE), 'No studio server availible');
 
 const errorCodeToSnackbar = (errorCode, msgOnOK) => {
   if (errorCode !== SyncStageSDKErrorCode.OK) {
